3435ce32ad10dc31990c9caf247646ba68ad97ce
zk_365 <1120429511@qq.com>
2017-03-28 15:07:42 +0800

    get

--- index.js
+++ index.js
@@ -16,16 +16,23 @@ request.get('https://asm.ca.com/en/ping.php', function (e, r, body) {
 		request.post('https://asm.ca.com/en/ping.php', function (e, r, body) {
 			if (e) {throw e}
 			var uid = body.match(/uid=\w+/)[0].slice(4);
-			console.log('find uid is ' + uid);
-			console.log('start get ips....');
-			for (var i = 1; i < 10; i++) {
-				var tempUrl = ipUrl + '?uid=' + uid + '&host=' + targetUrl + '&v=' + i;
-				console.log(tempUrl);
-				request.get(tempUrl, function(e, r, body) {
-					if (e) {throw e}
-					console.log(body.length)
-				})
-			}
+			var t = body.match(/t:\s\d+\.\d+/)[0].slice(3);
+			console.log('find uid is ' + uid + ' and t is ' + t);
+			console.log('start regist...');
+			request.post(ipUrl + '?uid=' + uid, function (e, r, body) {
+				console.log('regist complete!start get ips....')
+				for (var i = 1; i < 10; i++) {
+					var tempUrl = ipUrl + '?uid=' + uid + '&host=' + targetUrl + '&v=' + i;
+					console.log(tempUrl);
+					request.get(tempUrl, function(e, r, body) {
+						if (e) {throw e}
+						console.log(body)
+					})
+				}
+			}).form({
+				"host":targetUrl,
+				"t":t
+			})
 		}).form({
 			"vtt":vtt,
 			"varghost":targetUrl,
