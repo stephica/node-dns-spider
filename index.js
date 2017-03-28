f33d385ad49715aff8b7ea260dedf5c2059e5c73
zk_365 <1120429511@qq.com>
2017-03-28 13:17:40 +0800

    up

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var request = require('request');
+var env = require('jsdom').env;
 
 request.get('https://asm.ca.com/en/ping.php', function (e, r, body) {
-	var vtt = $(body).find('#vtt').val();
-	var vsectoken = $(body).find('#vsectoken').val();
-	console.log(vtt,vsectoken,1111111111111111111111111)
+	env(body,function (err, window) {
+		var $ = require('jquery')(window);
+		var vtt = $('#vtt').val();
+		var vsectoken = $('#vsectoken').val();
+		console.log(vtt,vsectoken,1111111111111111111111111);
 
-	request.post('https://asm.ca.com/en/ping.php', function (e, r, body) {
-		if (e) {throw e}
-		console.log(body)
-	}).form({
-		"vtt":vtt,
-		"varghost":"www.baidu.com",
-		"vhost":"_",
-		"vaction":"ping",
-		"ping":"Start",
-		"vsectoken":vsectoken
+		request.post('https://asm.ca.com/en/ping.php', function (e, r, body) {
+			if (e) {throw e}
+			console.log(body)
+		}).form({
+			"vtt":vtt,
+			"varghost":"www.baidu.com",
+			"vhost":"_",
+			"vaction":"ping",
+			"ping":"Start",
+			"vsectoken":vsectoken
+		})
 	})
 })
